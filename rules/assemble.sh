9be84c9f1c72437b9c8563e50b302eb7db10d77e
JV <mtngrown7@yahoo.com>
2024-07-29 11:32:32 -0700

    first compible version

---
+++ rules/assemble.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Initialize or clear the content of temp.txt
+> temp.txt
+
+# Loop through pages 9 to 12
+for i in {9..12}; do
+    # Concatenate left and right pages into temp.txt
+    cat "page-$i-left.txt" >> temp.txt
+    cat "page-$i-right.txt" >> temp.txt
+done
+
+sed -i '' 's/#/\\#/g' temp.txt
+
+cp temp.txt temp.tex
+
+echo "All files have been concatenated into temp.txt."
+
