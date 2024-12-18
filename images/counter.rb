98a4358ae76165358449cecd59fdc9b377d32cf7
JV <mtngrown7@yahoo.com>
2024-12-18 04:16:59 -0800

    counter class for basic details

---
+++ images/counter.rb
@@ -0,0 +1,23 @@
+# Base class for the wun
+class Counter
+  def counter_background(xml)
+    xml.rect(
+      x: '0',
+      y: '0',
+      width: counter_width,
+      height: counter_height,
+      fill: 'coral',
+      'fill-opacity': '0.3',
+      stroke: 'black',
+      'stroke-width': '1'
+    )
+  end
+
+  def counter_width
+    1024
+  end
+
+  def counter_height
+    1024
+  end
+end
